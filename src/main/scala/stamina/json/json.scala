804d4e4a6a78ab9b26c7aff0e203e8783ffa4c21
Age Mooij <age.mooij@gmail.com>
2015-01-05 16:33:18 +0100

    Made the implicit json util classes extend AnyVal

--- src/main/scala/stamina/json/json.scala
+++ src/main/scala/stamina/json/json.scala
@@ -6,11 +6,11 @@ import spray.json._
  * Adds some handy sugar around reading/writing json from/to ByteStrings.
  */
 package object json {
-  implicit class AnyWithJsonByteStringConversion[T](any: T) {
+  implicit class AnyWithJsonByteStringConversion[T](val any: T) extends AnyVal {
     def toJsonBytes(implicit writer: RootJsonWriter[T]): ByteString = ByteString(writer.write(any).compactPrint)
   }
 
-  implicit class ByteStringWithRootJsonReaderSupport(bytes: ByteString) {
+  implicit class ByteStringWithRootJsonReaderSupport(val bytes: ByteString) extends AnyVal {
     def fromJsonBytes[T](implicit reader: RootJsonReader[T]): T = {
       reader.read(JsonParser(ParserInput(bytes.toArray)))
     }
