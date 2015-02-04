36e9c1e81aa3369cad89d6d6d3071a4daca4021c
Age Mooij <age.mooij@gmail.com>
2015-02-04 20:12:40 +0100

    Moved PersistedEncoding to its own file

--- src/main/scala/stamina/StaminaAkkaSerializer.scala
+++ src/main/scala/stamina/StaminaAkkaSerializer.scala
@@ -1,8 +1,6 @@
 package stamina
 
-import scala.reflect._
 import scala.util._
-
 import akka.serialization._
 
 /**
@@ -43,51 +41,5 @@ abstract class StaminaAkkaSerializer(persisters: Persisters, encoding: Persisted
       case Success(deserialized) ⇒ deserialized
       case Failure(error)        ⇒ throw new UnrecoverableDataException(persisted, error)
     }
-  }
-}
-
-trait PersistedEncoding {
-  def identifier: Int
-  def writePersisted(persisted: Persisted): Array[Byte]
-  def readPersisted(bytes: Array[Byte]): Persisted
-}
-
-/**
- * Uses the following structure:
- *
- *   - length of key (4 bytes),
- *   - key bytes (n bytes),
- *   - version (4 bytes)
- *   - persisted data (n bytes)
- *
- */
-object DefaultPersistedEncoding extends PersistedEncoding {
-  implicit val byteOrder = java.nio.ByteOrder.LITTLE_ENDIAN
-  import java.nio.charset.StandardCharsets._
-
-  def identifier = 490303
-
-  def writePersisted(persisted: Persisted): Array[Byte] = {
-    val keyBytes = persisted.key.getBytes(UTF_8)
-
-    ByteString.
-      newBuilder.
-      putInt(keyBytes.length).
-      putBytes(keyBytes).
-      putInt(persisted.version).
-      append(persisted.bytes).
-      result.
-      toArray
-  }
-
-  def readPersisted(byteArray: Array[Byte]): Persisted = {
-    val bytes = ByteString(byteArray)
-    val keyLength = bytes.take(4).iterator.getInt
-    val (keyBytes, rest) = bytes.drop(4).splitAt(keyLength)
-    val (versionBytes, data) = rest.splitAt(4)
-    val key = keyBytes.utf8String
-    val version = versionBytes.iterator.getInt
-
-    Persisted(key, version, data)
   }
 }
