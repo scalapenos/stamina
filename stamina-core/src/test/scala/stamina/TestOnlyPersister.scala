78d05863f473ee902d33096d1ecc8f2bb45539f4
Age Mooij <age.mooij@gmail.com>
2015-02-06 14:34:33 +0100

    Added a test-only persister based on the Akka JavaSerializer to decouple the tests from the json implementation

---
+++ stamina-core/src/test/scala/stamina/TestOnlyPersister.scala
@@ -0,0 +1,21 @@
+package stamina
+
+import scala.reflect._
+import akka.actor._
+import akka.serialization._
+
+object TestOnlyPersister {
+  private val system = ActorSystem("TestOnlyPersister")
+  private val javaSerializer = new JavaSerializer(system.asInstanceOf[ExtendedActorSystem])
+  import javaSerializer._
+
+  def persister[T <: AnyRef: ClassTag](key: String): Persister[T, V1] = new JavaPersister[T](key)
+
+  private class JavaPersister[T <: AnyRef: ClassTag](key: String) extends Persister[T, V1](key) {
+    def persist(t: T): Persisted = Persisted(key, currentVersion, toBinary(t))
+    def unpersist(p: Persisted): T = {
+      if (canUnpersist(p)) fromBinary(p.bytes.toArray).asInstanceOf[T]
+      else throw new IllegalArgumentException("")
+    }
+  }
+}
