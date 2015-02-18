079d7ec30ef15f236bafffcaa35da2c2552aeb66
Age Mooij <age.mooij@gmail.com>
2015-02-18 11:15:52 +0100

    Fixed 2.10 macro compatibility by using the old deprecated methods instead of the new ones

--- stamina-json/src/main/scala/stamina/json/SprayJsonMacros.scala
+++ stamina-json/src/main/scala/stamina/json/SprayJsonMacros.scala
@@ -15,7 +15,7 @@ object SprayJsonMacroImpls {
     import c.universe._
 
     val tpe = weakTypeOf[T]
-    val methodNames = tpe.decls.toList collect {
+    val methodNames = tpe.declarations.toList collect {
       case method: MethodSymbol if method.isCaseAccessor ⇒ q"${method.name.toString}"
     }
 
@@ -26,7 +26,7 @@ object SprayJsonMacroImpls {
     // TODO: generate lazy formats for trees
     //       --> how to detect the need for laziness? Or just always generate lazy versions?
 
-    c.Expr[RootJsonFormat[T]](q"jsonFormat(${tpe.typeSymbol.companion}, ..$methodNames)")
+    c.Expr[RootJsonFormat[T]](q"jsonFormat(${tpe.typeSymbol.companionSymbol}, ..$methodNames)")
   }
 
   private def lazyRootFormat[T](format: ⇒ RootJsonFormat[T]) = new RootJsonFormat[T] {
