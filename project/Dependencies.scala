ecc11bfbe6f8d5e822cf358df444b9c9420f83ee
Age Mooij <age.mooij@gmail.com>
2015-10-15 16:49:26 +0200

    Upgraded to the latest bugfix release of json-lenses

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val akkaActor     = "com.typesafe.akka"  %% "akka-actor"       % "2.4.0"
   val sprayJson     = "io.spray"           %% "spray-json"       % "1.3.2"
-  val jsonLenses    = "net.virtual-void"   %% "json-lenses"      % "0.6.0"
+  val jsonLenses    = "net.virtual-void"   %% "json-lenses"      % "0.6.1"
   val scalatest     = "org.scalatest"      %% "scalatest"        % "2.2.5"
   val base64        = "me.lessis"          %% "base64"           % "0.2.0"
 
