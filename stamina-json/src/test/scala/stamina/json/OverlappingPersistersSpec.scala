c27bc6cb353eea2934b074c2fee157eb6a39763b
Arnout Engelen <arnouten@bzzt.net>
2016-02-19 12:18:15 +0100

    Move domain for a nicer exception message in the test

--- stamina-json/src/test/scala/stamina/json/OverlappingPersistersSpec.scala
+++ stamina-json/src/test/scala/stamina/json/OverlappingPersistersSpec.scala
@@ -5,16 +5,15 @@ import spray.json._
 import DefaultJsonProtocol._
 
 class OverlappingPersisterSpec extends StaminaJsonSpec {
+  import OverlappingPersisterSpecDomain._
+
+  implicit val payload1Format = jsonFormat1(Payload1)
+  implicit val payload2Format = jsonFormat1(Payload2)
+
+  implicit val eventPayload1Format = jsonFormat1(Event[Payload1])
+  implicit val eventPayload2Format = jsonFormat1(Event[Payload2])
+
   "The persisters construction DSL" should {
-    case class Event[P](payload: P)
-    case class Payload1(msg: String)
-    case class Payload2(value: Int)
-
-    implicit val payload1Format = jsonFormat1(Payload1)
-    implicit val payload2Format = jsonFormat1(Payload2)
-
-    implicit val eventPayload1Format = jsonFormat1(Event[Payload1])
-    implicit val eventPayload2Format = jsonFormat1(Event[Payload2])
 
     /** #43 In the future we might want to support this situation instead of failing at initialization time */
     "correctly handle overlapping persisters" in {
@@ -24,7 +23,7 @@ class OverlappingPersisterSpec extends StaminaJsonSpec {
           persister[Event[Payload2]]("payload2")
         )
       }
-      e.getMessage() should be("requirement failed: Overlapping persisters: Tags payload1, payload2 all persist class stamina.json.OverlappingPersisterSpec$$anonfun$1$Event$3")
+      e.getMessage() should be("requirement failed: Overlapping persisters: Tags payload1, payload2 all persist class stamina.json.OverlappingPersisterSpecDomain$Event")
 
       /**
        * When we actually want to support this situation, then this should work:
@@ -36,4 +35,10 @@ class OverlappingPersisterSpec extends StaminaJsonSpec {
        */
     }
   }
+}
+
+object OverlappingPersisterSpecDomain {
+  case class Event[P](payload: P)
+  case class Payload1(msg: String)
+  case class Payload2(value: Int)
 }
