3efdcc7550846e6a9431e05c4bf7efcd84d75c6f
Arnout Engelen <arnouten@bzzt.net>
2016-02-19 17:07:56 +0100

    Release version 0.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
 
   lazy val basicSettings = Seq(
     organization := "com.scalapenos",
-    version := "0.1.1-SNAPSHOT",
+    version := "0.1.2-SNAPSHOT",
     licenses := Seq("The MIT License (MIT)" -> url("http://opensource.org/licenses/MIT")),
     scalaVersion := "2.11.7",
     incOptions := incOptions.value.withNameHashing(true),
