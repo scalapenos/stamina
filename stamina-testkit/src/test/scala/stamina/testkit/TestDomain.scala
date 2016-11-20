863c13ce2f9528dda767fd79f237aa3960af7ed8
piotr.sliwa <me@psliwa.org>
2016-11-20 16:14:47 +0100

    [#56] define "fromVersionNumber" for samples in TestKit

--- stamina-testkit/src/test/scala/stamina/testkit/TestDomain.scala
+++ stamina-testkit/src/test/scala/stamina/testkit/TestDomain.scala
@@ -2,14 +2,9 @@ package stamina.testkit
 
 object TestDomain {
   type ItemId = Int
-  type CartId = Int
 
   case class Item(id: ItemId, name: String)
-  case class Cart(id: CartId, items: List[Item])
-  case class CartCreated(cart: Cart)
 
   val item1 = Item(1, "Wonka Bar")
   val item2 = Item(2, "Everlasting Gobstopper")
-  val cart = Cart(1, List(item1, item2))
-  val cartCreated = CartCreated(cart)
 }
