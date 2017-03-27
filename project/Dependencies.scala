35ea157226c8c61e00a60e985b495e17d057afcb
Arnout Engelen <arnouten@bzzt.net>
2017-03-27 16:45:56 +0200

    Scala 2.12 support (#66)
    Unfortunately our base64 dependency appears not to be available
    for scala 2.12 yet.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 import sbt.Keys._
 
 object Dependencies {
-  val akkaActor     = "com.typesafe.akka"  %% "akka-actor"       % "2.4.11"
+  val akkaActor     = "com.typesafe.akka"  %% "akka-actor"       % "2.4.17"
   val sprayJson     = "io.spray"           %% "spray-json"       % "1.3.2"
-  val jsonLenses    = "net.virtual-void"   %% "json-lenses"      % "0.6.1"
+  val jsonLenses    = "net.virtual-void"   %% "json-lenses"      % "0.6.2"
   val scalatest     = "org.scalatest"      %% "scalatest"        % "3.0.0"
   val base64        = "me.lessis"          %% "base64"           % "0.2.0"
 
   // Only used by the tests
-  val sprayJsonShapeless = "com.github.fommil" %% "spray-json-shapeless" % "1.1.0"
+  val sprayJsonShapeless = "com.github.fommil" %% "spray-json-shapeless" % "1.3.0"
 
   // Dependency scoping functions
   def compile   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "compile")
